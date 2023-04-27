cf97e3f78741b41bf39b87a5d9bf798a1e1c9653
dydami <106533691+dydami@users.noreply.github.com>
2023-04-27 12:17:10 +0100

    Add files via upload

---
+++ Student Account/src/test/java/com/example/studentaccount/StudentAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
