b80f4753abce9845024c5d26da99d802a6fa1fc7
dydami <106533691+dydami@users.noreply.github.com>
2023-04-27 04:50:51 +0100

    Create StudentAccountApplication.java

---
+++ StudentAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentAccountApplication.class, args);
+    }
+
+}
